58979178e05624e16649b65c8e9c68d59a7e66ac
bashforger <bash.forger@gmail.com>
2021-05-19 00:54:28 +0500

    Added basic UI

--- src/header.js
+++ src/header.js
@@ -5,12 +5,9 @@ const links = {
 };
 
 const header = () => {
-  // codefactory( _type, {_attr}, _content, _parent_id )
-  codefactory('header', { class: 'text-gray-600 body-font', id: 'header' }, '', 'content');
-  codefactory('div', { class: 'container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center', id: 'headercontainer' }, '', 'header');
-  codefactory('a', { class: 'title-font cursor-pointer font-medium items-center text-gray-900', href: '#' }, 'To-Dos', 'headercontainer');
-  codefactory('nav', { class: 'md:ml-auto flex flex-wrap items-center text-base justify-center', id: 'headerlinks' }, '', 'headercontainer');
-  Object.keys(links).forEach((l) => codefactory('a', { class: 'mr-5 hover:text-gray-900 cursor-pointer', id: l }, l, 'headercontainer'));
-};
+  codefactory('header', { class: 'text-gray-600 body-font my-3', id: 'header' }, '', 'content');
+  codefactory('div', { class: 'container mx-auto flex flex-wrap p-5 flex-col md:flex-row justify-center', id: 'headercontainer' }, '', 'header');
+  codefactory('a', { class: 'title-font cursor-pointer font-medium items-center text-gray-900 text-3xl', href: '#' }, 'Weather App', 'headercontainer');
+  };
 
 export default header;
