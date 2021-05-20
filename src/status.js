96bde24cd4c331b8bf28c0844e9261c2f1acb2cb
bashforger <bash.forger@gmail.com>
2021-05-20 06:59:54 +0500

    Remove comments

--- src/status.js
+++ src/status.js
@@ -32,11 +32,4 @@ export const status = () => {
     codefactory('span', { class: 'text-gray-500' }, `Deg: ${window.weather.deg}`, 'wind-details');
     codefactory('span', { class: 'text-gray-500' }, `Speed: ${window.weather.speed}`, 'wind-details');
   }
-  // <div class='h-full flex items-center border-gray-200 border p-4 rounded-lg'>
-  //   <img alt='team' class='w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4' src='https://dummyimage.com/80x80'>
-  //   <div class='flex-grow'>
-  //     <h2 class='text-gray-900 title-font font-medium'>Holden Caulfield</h2>
-  //     <p class='text-gray-500'>UI Designer</p>
-  //   </div>
-  // </div>
 };
